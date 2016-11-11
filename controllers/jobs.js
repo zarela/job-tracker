8b62bfca4886a0a90f275fa4206c8b81bcf13368
Zarela Velasquez <zarelavp@yahoo.com>
2016-11-11 13:03:04 -0500

    Added forms to create jobs

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -17,5 +17,47 @@ router.get('/', function(req, res){
   })
 });
 
+router.get('/:jobId', function(req, res){
+  Job.findOne({_id: req.params.jobId})
+  .then(function(job){
+    res.json({job: job});
+  })
+  .catch(function(err){
+    res.json(500, `ERROR: ${err}`)
+  });
+});
+
+//Creating a new job
+router.post('/', function(req, res){
+  var newJob;
+
+  Job.create({
+    position: req.body.position,
+    company: req.body.company,
+    jobLink: req.body.jobLink,
+    interestLevel: req.body.interestLevel,
+    applied: req.body.applied || false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    notes: req.body.notes
+  })
+  .then(function(job){
+    console.log('New job created --->', job)
+    newJob = job;
+  })
+  .then(function(){
+    return Job.find({}).exec();
+  })
+  .then(function(todos){
+    console.log('All jobs so far --->', jobs)
+    res.json({jobs: jobs, job: newJob});
+  })
+  .catch(function(err){
+    res.json(400, err)
+  })
+});
+
+
+
 
 module.exports = router;
--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,43 @@
 (function(){
   angular.module('job', []).controller('MainCtrl', function($http){
 
+  var self = this;
+
+  $http.get('/jobs')
+  .then(function(response){
+    console.log("Self works")
+    self.jobs = response.data.jobs
+  })
+  .catch(function(err){
+    console.log(err)
   });
 
+//Keeping Track of App State
+//============================
+this.isCreating = false;
+
+function startCreating(){
+  this.isCreating = true;
+}
+
+//CRUD Logic
+//==========================
+function addJob(newJob){
+  console.log(newJob);
+
+  $http.post('/jobs', newJob)
+  .then(function(response){
+    console.log(response)
+    self.jobs = response.data.jobs;
+    newJob.description = '';
+  })
+  .catch(function(err){
+    console.log(err)
+  });
+}
+
+
+  }); //End
 })();
 
 console.log("It works!");
